b8500d3e1f7854b9ca2006c091ea2987c939b9e9
Julio Moreira B. Barros <julio.barros@playkids.com>
2019-03-27 15:23:01 -0300

    use spek

---
+++ src/test/kotlin/com/movile/playkids/ui/test/ChromeBojackAuthenticationTest.kt
@@ -0,0 +1,9 @@
+package com.movile.playkids.ui.test
+
+import com.movile.playkids.ui.test.TestCommons.chromeDriver
+
+/**
+ * @author Júlio Moreira Blás de Barros (julio.barros@movile.com)
+ * @since 3/27/19
+ */
+object ChromeBojackAuthenticationTest: BojackAuthenticationTest(chromeDriver)
